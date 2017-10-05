86e3dfd655961d3109a5cd8be7c64ae484708137
AndreaBenefiel <32530230+AndreaBenefiel@users.noreply.github.com>
2017-10-04 19:13:45 -0600

    Add files via upload

---
+++ fizzbuzz.java
@@ -0,0 +1,32 @@
+
+/**
+Fizzbuzz code challenge
+ */
+public class fizzbuzz
+{
+ 
+    public static void fizzbuzz()
+    {
+        
+        for (int i = 1; i<101 ; i++){           //count from 1-100
+        if(i%3 == 0 && i%5==0){                 //divisible by 3 and 5
+            System.out.print("Fizzbuzz");
+            System.out.print("\n");
+        }
+        else if(i%3==0 && i %5 != 0){           //divisible by 3 only
+            System.out.print("Fizz");
+            System.out.print("\n");
+        }
+        else if(i%3!=0 && i %5 == 0){           //divisible by 5 only
+            System.out.print("Buzz");
+            System.out.print("\n");
+        }
+        else{                                   //divisible by neither
+            System.out.print(i);
+            System.out.print("\n");
+        }
+        }
+        
+        return ;
+    }
+}
