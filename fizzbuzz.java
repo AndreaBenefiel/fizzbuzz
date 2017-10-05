62cd329ca9dc2b5dd78ebec603d6e4b8236f728d
AndreaBenefiel <32530230+AndreaBenefiel@users.noreply.github.com>
2017-10-04 19:21:00 -0600

    Update fizzbuzz.java

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -1,6 +1,6 @@
 
-/**
-Fizzbuzz code challenge
+/*
+FizzBuzz code challenge
  */
 public class fizzbuzz
 {
@@ -10,7 +10,7 @@ public class fizzbuzz
         
         for (int i = 1; i<101 ; i++){           //count from 1-100
         if(i%3 == 0 && i%5==0){                 //divisible by 3 and 5
-            System.out.print("Fizzbuzz");
+            System.out.print("FizzBuzz");
             System.out.print("\n");
         }
         else if(i%3==0 && i %5 != 0){           //divisible by 3 only
